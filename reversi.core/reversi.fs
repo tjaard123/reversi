cb930f5619cbcd9291c5e1892d8c555d4c796f2e
Tjaard du Plessis <tjaard123@gmail.com>
2018-12-21 13:19:42 +0200

    Basic tests working

--- reversi.core/reversi.fs
+++ reversi.core/reversi.fs
@@ -1,40 +1,38 @@
-namespace Reversi
+module Reversi.Core
 
-module Core =
+type Direction = North | NorthWest | West | SouthWest | South | SouthEast | East | NorthEast
 
-    type Direction = North | NorthWest | West | SouthWest | South | SouthEast | East | NorthEast
+let c2i = System.Globalization.CharUnicodeInfo.GetDigitValue
+let pos2xy (pos:string) = (int pos.[0] - int 'A', (c2i pos.[1]) - 1)
 
-    let c2i = System.Globalization.CharUnicodeInfo.GetDigitValue
-    let pos2xy (pos:string) = (int pos.[0] - int 'A', (c2i pos.[1]) - 1)
+let at (board: string[]) pos =
+    match pos2xy pos with
+    | (x, y) when x >= 0 && x <= 7 && y >= 0 && y <= 7 -> Some(board.[y].[x])
+    | _ -> None
 
-    let at (board: string[]) pos =
-        match pos2xy pos with
-        | (x, y) when x >= 0 && x <= 7 && y >= 0 && y <= 7 -> Some(board.[y].[x])
-        | _ -> None
+let advance (pos : string) direction n = 
+    match direction with
+    | North -> string pos.[0] + string ((c2i pos.[1]) - n)
+    | NorthWest -> string (char (int pos.[0] + n)) + string ((c2i pos.[1]) - n)
+    | West -> string (char (int pos.[0] + n)) + string pos.[1]
+    | SouthWest -> string (char (int pos.[0] + n)) + string ((c2i pos.[1]) + n)
+    | South -> string pos.[0] + string ((c2i pos.[1]) + n)
+    | SouthEast -> string (char (int pos.[0] - n)) + string ((c2i pos.[1]) + n)
+    | East -> string (char (int pos.[0] - n)) + string pos.[1]
+    | NorthEast -> string (char (int pos.[0] - n)) + string ((c2i pos.[1]) - n)
 
-    let advance (pos : string) direction n = 
-        match direction with
-        | North -> string pos.[0] + string ((c2i pos.[1]) - n)
-        | NorthWest -> string (char (int pos.[0] + n)) + string ((c2i pos.[1]) - n)
-        | West -> string (char (int pos.[0] + n)) + string pos.[1]
-        | SouthWest -> string (char (int pos.[0] + n)) + string ((c2i pos.[1]) + n)
-        | South -> string pos.[0] + string ((c2i pos.[1]) + n)
-        | SouthEast -> string (char (int pos.[0] - n)) + string ((c2i pos.[1]) + n)
-        | East -> string (char (int pos.[0] - n)) + string pos.[1]
-        | NorthEast -> string (char (int pos.[0] - n)) + string ((c2i pos.[1]) - n)
-
-    let rec IsValidMove board direction pos i player =
-        printfn "i%d: %s > %A" i pos (at board pos)
-        match i with
-        | 0 -> IsValidMove board direction (advance pos direction 1) (i + 1) player
-        | 1 ->
-            match at board pos with
-            | Some(v) when v = player || v = '-' -> false
-            | Some(_) -> IsValidMove board direction (advance pos direction 1) (i + 1) player
-            | None -> false
-        | _ ->
-            match at board pos with
-            | Some(v) when v = player -> true
-            | Some(v) when v = '-' -> false
-            | Some(_) -> IsValidMove board direction (advance pos direction 1) (i + 1) player
-            | None -> false
+let rec IsValidMove board direction pos i player =
+    printfn "i%d: %s > %A" i pos (at board pos)
+    match i with
+    | 0 -> IsValidMove board direction (advance pos direction 1) (i + 1) player
+    | 1 ->
+        match at board pos with
+        | Some(v) when v = player || v = '-' -> false
+        | Some(_) -> IsValidMove board direction (advance pos direction 1) (i + 1) player
+        | None -> false
+    | _ ->
+        match at board pos with
+        | Some(v) when v = player -> true
+        | Some(v) when v = '-' -> false
+        | Some(_) -> IsValidMove board direction (advance pos direction 1) (i + 1) player
+        | None -> false
--- reversi.test/Tests.fs
+++ reversi.test/Tests.fs
@@ -1,8 +1,35 @@
-module Tests
+module Reversi.Tests
+
+// dotnet test
 
 open System
 open Xunit
+open Reversi.Core
+
+let board = [|
+    "----oxox";
+    "---oxx-o";
+    "-oxxxxxo";
+    "--------"; // oooooooo
+    "--------";
+    "--------";
+    "--------";
+    "--------";
+|]
 
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``A4 North`` () = Assert.False(IsValidMove board North "A4" 0 'o')
+[<Fact>]
+let ``B4 North`` () = Assert.False(IsValidMove board North "B4" 0 'o')
+[<Fact>]
+let ``C4 North`` () = Assert.False(IsValidMove board North "C4" 0 'o')
+[<Fact>]
+let ``D4 North`` () = Assert.True(IsValidMove board North "D4" 0 'o')
+[<Fact>]
+let ``E4 North`` () = Assert.True(IsValidMove board North "E4" 0 'o')
+[<Fact>]
+let ``F4 North`` () = Assert.False(IsValidMove board North "F4" 0 'o')
+[<Fact>]
+let ``G4 North`` () = Assert.False(IsValidMove board North "G4" 0 'o')
+[<Fact>]
+let ``H4 North`` () = Assert.False(IsValidMove board North "H4" 0 'o')
